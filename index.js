0df9543e05f202c0336b6da6d3c8cc8b491d01fa
Sharma <priyanka.sharma03@sap.com>
2018-08-11 18:06:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express  = require("express");
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const axios = require('axios');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+let url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=";
+const secondParamter = "&types=geocode&location=28.644800,77.216721&radius=500&key=AIzaSyBsxbN_fwHVW4II9wHw0s4d9z3eKncVG4Y"
+
+// creating server
+app.listen("4000", function(){
+    console.log("server started at port", 4000)
+})
+
+app.post('/', function(req, res){
+    console.log('body is:',req.body)
+    res.send("hello, i am  home page")
+})
+
+app.get('/login', function(request, response){
+    response.send("Login Page")
+})
